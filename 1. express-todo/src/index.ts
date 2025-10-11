bd69674617f2c122cc2b788b48443db675caa59a
thearnabsaha <thearnabsaha201@gmail.com>
2025-10-11 15:17:19 +0530

    chore: update .gitignore to include .env file refactor: remove unnecessary CORS options from index.ts

--- 1. express-todo/src/index.ts
+++ 1. express-todo/src/index.ts
@@ -20,10 +20,6 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
