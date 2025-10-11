6e509cb9f6a9649e7091d679cf62008dab337385
thearnabsaha <thearnabsaha201@gmail.com>
2025-10-11 17:27:15 +0530

    fix: update CI workflow to restart application after deployment refactor: simplify CORS configuration in Express app fix: correct response message in root route of Express app

--- 1. express-todo/src/index.ts
+++ 1. express-todo/src/index.ts
@@ -14,12 +14,7 @@ const morganFormat = ':method :url :status :response-time ms';
 app.use(morgan(morganFormat));
 app.use(helmet());
 
-app.use(cors({
-    origin: "http://localhost:5173", // allow your React frontend
-    credentials: true,              // allow cookies if needed
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
@@ -40,7 +35,7 @@ app.get('/health', async (req, res) => {
     res.status(200).json(healthcheck);
 });
 app.get('/', (req, res) => {
-    res.send('Its a beautiful world');
+    res.send('Its a beautiful world!');
 });
 // GET all todos
 app.get("/todos", (req, res) => {
